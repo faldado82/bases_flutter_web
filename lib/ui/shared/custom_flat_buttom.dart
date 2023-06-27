d636aba0fa23af037a5758972e3a641a45b021d5
faldado82 <faldado82@gmail.com>
2023-06-27 19:31:04 -0300

    first commit

---
+++ lib/ui/shared/custom_flat_buttom.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomFlatButtom extends StatelessWidget {
+  final String text;
+  final Color color;
+  final Function onPressed;
+
+  const CustomFlatButtom(
+      {super.key,
+      required this.text,
+      this.color = Colors.red,
+      required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: TextButton.styleFrom(foregroundColor: color),
+      onPressed: () => onPressed(),
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 25),
+        ),
+      ),
+    );
+  }
+}
